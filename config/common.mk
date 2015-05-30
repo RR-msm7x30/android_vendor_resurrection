61e25cf24c8c032201d5be046893b273e4eb9a40
Călin Neamţu <nilac8991@gmail.com>
2015-05-30 19:53:37 +0200

    Added custom msm7x30 changelog
    -ChangeLog which it will be used for propietary changes

--- config/common.mk
+++ config/common.mk
@@ -73,6 +73,10 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
+# Copy msm custom changelog to the device
+PRODUCT_COPY_FILES += \
+    vendor/cm/CHANGELOG_MSM.mkdn:system/etc/CHANGELOG-MSM7X30.txt
+
 # Copy over the changelog to the device
 PRODUCT_COPY_FILES += \
     vendor/cm/CHANGELOG.mkdn:system/etc/CHANGELOG-RR.txt
