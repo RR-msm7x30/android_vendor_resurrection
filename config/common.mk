6efedfa3b936061dbe023741591a3a8f6140459b
nilac8991 <nilac8991@gmail.com>
2015-07-27 21:36:48 +0200

    Build OTAUpdates directly from sources
    - Removed old prebuilt OTAUpdates from prebuilt files
    
    Change-Id: Id8a9a0f0fea0bdcc7ebbd3ff97b36fa53e6d7123

--- config/common.mk
+++ config/common.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
     LockClock \
     CMAccount \
     CMHome \
-    OTACenter
+    OTAUpdates
 
 # SuperSU
 PRODUCT_COPY_FILES += \
@@ -175,7 +175,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	vendor/cm/prebuilt/msm7x30/CPU/CPUstats.apk:system/app/CPUstats.apk \
 	vendor/cm/prebuilt/msm7x30/CPU/CPUsettings.apk:system/app/CPUsettings.apk \
-	vendor/cm/prebuilt/msm7x30/OTAUpdates/OTAUpdates.apk:system/priv-app/OTAUpdates.apk \
 	vendor/cm/prebuilt/msm7x30/QuickBoot/QuickBoot.apk:system/priv-app/QuickBoot.apk
 
 # CM Platform Library
